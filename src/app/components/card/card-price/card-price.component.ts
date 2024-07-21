4ec447f90bac7b287ffa2c99f1d0f906b32b489f
caiorodri <caiodossantosrodrigues@gmail.com>
2024-07-21 16:45:00 -0300

    Finalização da primeira versão

--- src/app/components/card/card-price/card-price.component.ts
+++ src/app/components/card/card-price/card-price.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card-price',
@@ -7,5 +7,16 @@ import { Component } from '@angular/core';
 })
 export class CardPriceComponent {
 
+  @Input()
+  public consoles: string[] = ["PS4"];
+
+  @Input()
+  public gameValue: number = 99.99;
+  
+  @Input()
+  public typeCoin: string = "R$";
+  
+  @Input()
+  public gameCardTitle: string = "Play It Now!";
 
 }
--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -9,5 +9,20 @@ export class CardComponent {
 
   @Input()
   public gameImage: string = "";
+
+  @Input()
+  gameLabel: string = "Exclusive";
+
+  @Input()
+  public consoles: string[] = ["PS3", "PS4"];
+
+  @Input()
+  public gameValue: number = 99.99;
+  
+  @Input()
+  public typeCoin: string = "R$";
+  
+  @Input()
+  public gameCardTitle: string = "Play It Now!";
   
 }
